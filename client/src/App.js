ddae316c1a27466fcc63da3461fcbae18828b104
LeDiepTuyetNhuB1910119 <nhub1910119@gmail.com>
2023-02-11 14:48:14 +0700

    tạo view cơ bản

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./components/layout/Landing";
+import Home from "./views/Home";
+import About from "./views/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </Router>
   );
 }
 
