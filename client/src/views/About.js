d63633d5c9040b224aab6c10f3ccace27f50733c
LeDiepTuyetNhuB1910119 <nhub1910119@gmail.com>
2023-02-11 15:04:12 +0700

    hoàn thành trang about

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import NavbarMenu from "../components/layout/NavbarMenu";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <>
+      <NavbarMenu />
+      <Row className="mt-5" style={{ marginRight: 0 }}>
+        <Col className="text-center">
+          <Button
+            variant="primary"
+            href="https://www.facebook.com/tuyngoc990"
+            size="lg"
+          >
+            Visit my facebook for more information
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default About;
