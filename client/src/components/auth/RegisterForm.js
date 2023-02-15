b036210cbb16a84abe49397d478db9e5724ba4bc
LeDiepTuyetNhuB1910119 <nhub1910119@gmail.com>
2023-02-15 13:05:09 +0700

    xử lý register

--- client/src/components/auth/RegisterForm.js
+++ client/src/components/auth/RegisterForm.js
@@ -1,7 +1,116 @@
-import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import { Link } from "react-router-dom";
+
+import { useContext, useState } from "react";
+
+import AlertMessage from "../layout/AlertMessage";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const RegisterForm = () => {
-  return <h1>Register</h1>;
+  // context
+  const { registerUser } = useContext(AuthContext);
+
+  // login state
+  const [registerForm, setRegisterForm] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  // alert state
+  const [alert, setAlert] = useState(null);
+
+  // destructuring username, password từ loginForm (state)
+  const { username, password, confirmPassword } = registerForm;
+
+  // function onChangeLoginForm
+  const onChangeRegisterForm = (event) => {
+    setRegisterForm({
+      ...registerForm,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // function on submit register
+  const register = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      setAlert({ type: "danger", message: "Passwords do not match" });
+      setTimeout(() => setAlert(null), 5000);
+      return;
+    }
+
+    try {
+      const registerData = await registerUser(registerForm);
+      if (!registerData.success) {
+        setAlert({ type: "danger", message: registerData.message });
+        setTimeout(() => setAlert(null), 5000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // return
+  return (
+    <>
+      <h1 className="text-warning">Register</h1>
+      <Form className="my-4" onSubmit={register}>
+        <AlertMessage info={alert} />
+        <Form.Group className="mb-2">
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            name="username"
+            required
+            value={username}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+            value={password}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            required
+            value={confirmPassword}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+        <Button variant="success" type="submit">
+          Register
+        </Button>
+      </Form>
+      <p>
+        Already have an account?
+        <Link to="/login">
+          <Button variant="info" size="sm" className="ml-2">
+            Login
+          </Button>
+        </Link>
+      </p>
+      <p>
+        <Link to="/home">
+          <Button variant="info" size="sm" className="ml-2">
+            Back to home page
+          </Button>
+        </Link>
+      </p>
+    </>
+  );
 };
 
 export default RegisterForm;
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -70,6 +70,25 @@ const AuthContextProvider = ({ children }) => {
     }
   };
 
+  // Register
+  const registerUser = async (userForm) => {
+    try {
+      const response = await axios.post(`${apiUrl}/auth/register`, userForm);
+      if (response.data.success)
+        localStorage.setItem(
+          LOCAL_STORAGE_TOKEN_NAME,
+          response.data.accessToken
+        );
+
+      await loadUser();
+
+      return response.data;
+    } catch (error) {
+      if (error.response.data) return error.response.data;
+      else return { success: false, message: error.message };
+    }
+  };
+
   // Logout
   const logoutUser = () => {
     localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME);
@@ -85,6 +104,7 @@ const AuthContextProvider = ({ children }) => {
     loadUser,
     loginUser,
     logoutUser,
+    registerUser,
   };
 
   // return
