86e6260aefa88a0c17eac649421dd37bccf6180e
LeDiepTuyetNhuB1910119 <nhub1910119@gmail.com>
2023-02-20 07:37:43 +0700

    cập nhật book model

--- server/app/models/BookModel.js
+++ server/app/models/BookModel.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const BookSchema = new Schema(
   {
-    title: { type: String, unique: true, required: true },
+    title: { type: String, required: true },
     description: { type: String },
     user: { type: Schema.Types.ObjectId, ref: "users" },
     category: { type: Schema.Types.ObjectId, ref: "categories" },
+    image: { type: Object, required: true },
   },
   {
     timestamps: true,
