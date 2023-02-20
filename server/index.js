88491df70e5bff1c1c3bfb694c72cef73f3ae6ed
LeDiepTuyetNhuB1910119 <nhub1910119@gmail.com>
2023-02-20 07:32:54 +0700

    tạo file cloudinary, cập nhật file .env

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const connectDB = async () => {
 connectDB();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
